c276912e17222d5dc2de419b28603c4daa6f58cf
Muham <Muham@LAPTOP-68KSR8VJ>
2020-05-21 09:50:34 +0800

    Razin 21 May 09:50
    Changes reflected in the Bird class

--- com.opt.java.singteltest/src/singteltest/Solution.java
+++ com.opt.java.singteltest/src/singteltest/Solution.java
@@ -6,15 +6,6 @@ class Animal {
 	}
 }
 
-class Bird extends Animal {
-	void fly() {
-		System.out.println("I am flying");
-	} 
-	void sing() {
-		System.out.println("I am Singing");
-	}
-}
-
 public class Solution {
 	public static void main(String[] args) {
 		Bird bird = new Bird();
