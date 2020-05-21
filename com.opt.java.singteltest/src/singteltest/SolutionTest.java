72134e98d7b00c0d058ff12b8d1bf5b737f553f1
Muham <Muham@LAPTOP-68KSR8VJ>
2020-05-21 11:33:01 +0800

    Razin 21 May 11:32
    Made simple test cases to for unit testing

--- com.opt.java.singteltest/src/singteltest/SolutionTest.java
+++ com.opt.java.singteltest/src/singteltest/SolutionTest.java
@@ -1,10 +1,21 @@
 package singteltest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
+	
+	//Since the methods are returning string test whether they output the correct string
+	//Also good to test that they return the message or any message
+	Bird refD = new Duck();
 
 	@Test
-	public void testOuput() {}
+	public void testNotNull() {
+		Assert.assertNotNull(refD);
+	}
+	@Test
+	public void testOuput() {
+		Assert.assertEquals("Quack Quack",refD.sing());
+	}
 
 }
