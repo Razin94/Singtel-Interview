df7a7eb7e05bc760b564e220c3ba1ab3a0a5c159
Muham <Muham@LAPTOP-68KSR8VJ>
2020-05-21 11:00:00 +0800

    Razin 21 May 10:59
    Changes is reflected in other classes and interfaces

--- com.opt.java.singteltest/src/singteltest/Solution.java
+++ com.opt.java.singteltest/src/singteltest/Solution.java
@@ -2,8 +2,15 @@ package singteltest;
 
 public class Solution {
 	public static void main(String[] args) {
+		//References to point to the correct classes and methods
+		Parrot refPWD = new ParrotWithDog();
+		Parrot refPWC = new ParrotWithCat();
+		
 		System.out.println(Animal.walk);
 		System.out.println(Bird.Fly);
+		//Each reference is pointing to a different parrot
+		System.out.println(refPWD.sing());
+		System.out.println(refPWC.sing());
 		}
 
 }
