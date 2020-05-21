be4b2ccf7717a51f2b19c81d98fd01da74ec40e8
Muham <Muham@LAPTOP-68KSR8VJ>
2020-05-21 10:07:52 +0800

    Razin 21 May 10:07
    The Bird class now implements the Animal interface. Rooster Model has
    been included

--- com.opt.java.singteltest/src/singteltest/Bird.java
+++ com.opt.java.singteltest/src/singteltest/Bird.java
@@ -1,6 +1,10 @@
 package singteltest;
 
-public class Bird extends Animal {
+public class Bird implements Animal {
+	@Override
+	public void walk() {
+		System.out.println("I am walking");
+	}
 	void fly() {
 		System.out.println("I am flying");
 	} 
@@ -9,7 +13,7 @@ public class Bird extends Animal {
 	}
 }
 
-class Duck extends Animal {
+class Duck extends Bird {
 	void fly() {
 		System.out.println("I am flying");
 	}
@@ -21,11 +25,22 @@ class Duck extends Animal {
 	}
 }
 
-class Chicken extends Animal {
+class Chicken extends Bird {
 	void fly() {
 		System.out.println("I cannot fly");
 	}
 	void sing() {
 		System.out.println("Cluck Cluck");
+	}
+}
+
+class Rooster extends Bird {
+	//Implementing a model of Rooster with inheritance gives it a relationship to either Bird or Rooster
+	//Creating the class on it's own is possible but it does not give a sense of the code not being done properly
+	void fly() {
+		System.out.println("I cannot fly");
+	}
+	void sing() {
+		System.out.println("Cock-a-doodle-doo");
 	}
 }
