e90f77b14897e4f2d87f1bd325964ba7e3c7dd32
Muham <Muham@LAPTOP-68KSR8VJ>
2020-05-21 10:31:21 +0800

    Razin 21 May 10:30
    Changed the bird class into an interface, extended the animal interface
    as well. Allowing classes to inherit all relevant abstract methods

--- com.opt.java.singteltest/src/singteltest/Bird.java
+++ com.opt.java.singteltest/src/singteltest/Bird.java
@@ -1,46 +1,69 @@
 package singteltest;
 
-public class Bird implements Animal {
+public interface Bird extends Animal {
+	public String Walk = "I am walking";
+	public String Fly = "I am flying";
+	public String sing = "I am Singing";
+	void fly(); 
+	void sing();
+}
+
+class Duck implements Bird {
 	@Override
 	public void walk() {
 		System.out.println("I am walking");
 	}
-	void fly() {
-		System.out.println("I am flying");
-	} 
-	void sing() {
-		System.out.println("I am Singing");
-	}
-}
 
-class Duck extends Bird {
-	void fly() {
+	@Override
+	public void fly() {
 		System.out.println("I am flying");
 	}
-	void sing() {
+
+	@Override
+	public void sing() {
 		System.out.println("Quack Quack");
 	}
-	void ownMove() {
-		System.out.println("I can Swim");
+	
+	public void swim() {
+		System.out.println("I am swimming");
 	}
 }
 
-class Chicken extends Bird {
-	void fly() {
+class Chicken implements Bird {
+
+	@Override
+	public void walk() {
+		System.out.println("I am walking");
+	}
+
+	@Override
+	public void fly() {
 		System.out.println("I cannot fly");
 	}
-	void sing() {
+
+	@Override
+	public void sing() {
 		System.out.println("Cluck Cluck");
 	}
+	
 }
 
-class Rooster extends Bird {
+class Rooster implements Bird {
 	//Implementing a model of Rooster with inheritance gives it a relationship to either Bird or Rooster
-	//Creating the class on it's own is possible but it does not give a sense of the code not being done properly
-	void fly() {
-		System.out.println("I cannot fly");
+		//Creating the class on it's own is possible but it does not give a sense of the code not being done properly
+	
+	@Override
+	public void walk() {
+		System.out.println("I am walking");
 	}
-	void sing() {
+
+	@Override
+	public void fly() {
+		System.out.println(" I cannot fly");
+	}
+
+	@Override
+	public void sing() {
 		System.out.println("Cock-a-doodle-doo");
 	}
 }
